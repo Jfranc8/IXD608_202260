09a1f9891caa4c7339367f2ac9184394844e3f60
Jfranc8 <108033774+Jfranc8@users.noreply.github.com>
2022-07-15 22:34:00 -0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title> Jessica's Indoor Houseplants </title>
+	<link rel="stylesheet" href="lib/css/styleguide.css">
+	<link rel="stylesheet" href="css/storetheme.css">
+
+	<meta name="viewport" conttent="width=device-width">
+
+<link rel="stylesheet" href="lib/css/styleguide.css">
+<link rel="stylesheet" href="lib/css/gridsystem.css">
+<link rel="stylesheet" href="css/storetheme.css"></head>
+
+<body>
+	<header class="navbar">
+	<div class="container display-flex">
+ 		<div class="flex-none">
+	   		<h1>Jessica's Indoor Houseplants</h1>
+		</div>
+	
+		<div class="flex-stretch"></div>
+
+		<nav class="flex-none nav">
+       		<nav class="nav">
+	    		<ul class="container display-flex">
+		    		<li> <a href="index.php">Home</a></li>
+		    		<li> <a href="about.php">About</a></li>
+		    		<li> <a href="product_list.php">Shop</a></li>
+		    		<li> <a href="contact.php">Contact</a></li>
+	     		</ul>
+       		</nav>
+       	</nav>
+    </div>
+</header>
+	<div class="container">
+		<article id="article1" class="article card soft">
+			<div class="display-flex flex-align-center">
+				<div class="flex-stretch">
+		      		<h2>Get in touch with us</h2>
+		    	</div>
+			</div>
+		
+			<div class="article-body">
+				<ul>
+					<li>Call Us: (415) 837-0937 </li>
+					<li>Email: jessicas.indoorhouseplants@gmail.com </li>
+					<li>Address: San Francisco, CA 94105</li>
+				</ul>
+			</div>
+		</article>
+	</div>
+</body>
+</html>
