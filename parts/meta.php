7303c1e0052c6df67b481f194a97eb3d4fe61e5d
Jfranc8 <108033774+Jfranc8@users.noreply.github.com>
2022-07-21 19:01:00 -0700

    Add files via upload

---
+++ parts/meta.php
@@ -0,0 +1,8 @@
+
+<meta name="viewport" content="width=device-width">
+
+<base href="http://jessicafranco.co/aau/wnm608/franco.jessica/">
+
+<link rel="stylesheet" type="text/css" href="lib/css/styleguide.css">
+<link rel="stylesheet" type="text/css" href="lib/css/gridsystem.css">
+<link rel="stylesheet" type="text/css" href="css/storetheme.css">
---
+++ parts/navbar.php
@@ -0,0 +1,21 @@
+
+
+<header class="navbar">
+	<div class="container display-flex">
+ 		<div class="flex-none">
+	   		<h1>Jessica's Indoor Houseplants</h1>
+		</div>
+	
+		<div class="flex-stretch"></div>
+		<nav class="flex-none nav">
+       		<nav class="nav">
+	    		<ul class="container display-flex">
+		    		<li> <a href="index.php">Home</a></li>
+		    		<li> <a href="about.php">About</a></li>
+		    		<li> <a href="product_list.php">Shop</a></li>
+		    		<li> <a href="contact.php">Contact</a></li>
+	     		</ul>
+       		</nav>
+       	</nav>
+    </div>
+</header>
