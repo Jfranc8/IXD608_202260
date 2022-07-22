1d07809c7e4ffbfd5154d6f4f86d0d59f1a4bef1
Jfranc8 <108033774+Jfranc8@users.noreply.github.com>
2022-07-21 18:59:16 -0700

    Add files via upload

---
+++ admin/users.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>User Admin Page</title>
+
+    
+<meta name="viewport" conttent="width=device-width">
+
+<base href="http://jessicafranco.co/aau/wnm608/franco.jessica/">
+
+<link rel="stylesheet" href="lib/css/styleguide.css">
+<link rel="stylesheet" href="lib/css/gridsystem.css">
+<link rel="stylesheet" href="css/storetheme.css"></head>
+<body>
+    <header class="navbar">
+        <div class="container display-flex">
+            <div class="flex-none">
+                <h1>User Admin</h1>
+            </div>
+            <div class="flex-stretch"></div>
+            <nav class="nav nav-flex flex-none">
+                <ul>
+                    <li><a href="aau/wnm608/franco.jessica/admin/users.php">User List</a></li>
+                    <li><a href="aau/wnm608/franco.jessica/admin/users.php?id=new">Add New User</a></li>
+                </ul>
+            </nav>  
+        </div>
+    </header>
+
+    <div class="container">
+        <div class="card soft">
+
+            
+            
+            <h2>User List</h2>
+
+            <nav class="nav">
+
+                <ul>
+                    <li>
+                           <a href='admin/users.php?id=0'>Hamilton Cline</a>
+                        </li><li>
+                           <a href='admin/users.php?id=1'>Phoebe Buffay</a>
+                        </li><li>
+                           <a href='admin/users.php?id=2'>Joey Tribbiani</a>
+                        </li><li>
+                           <a href='admin/users.php?id=3'>Chandler Bing</a>
+                        </li><li>
+                           <a href='admin/users.php?id=4'>Monica Gellar</a>
+                        </li>  
+                        <a href='admin/users.php?id=5'>Rose Gellar</a>
+                        </li>         
+               </ul>
+            </nav>
+
+                    </div>
+    </div>
+</body>
+</html>
