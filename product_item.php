6f739293393dd9cbfc87ba1cf8d93c4911f51f4e
Jfranc8 <108033774+Jfranc8@users.noreply.github.com>
2022-07-24 03:12:22 -0700

    Add files via upload

---
+++ product_item.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Product Item</title>
+
+	
+<meta name="viewport" content="width=device-width">
+
+<base href="http://jessicafranco.co/wnm608/franco.jessica/">
+
+<link rel="stylesheet" href="lib/css/styleguide.css">
+<link rel="stylesheet" href="lib/css/gridsystem.css">
+<link rel="stylesheet" href="css/storetheme.css"></head>
+<body>
+	<header class="navbar">
+	<div class="container display-flex">
+ 		<div class="flex-none">
+	   		<h1>Product Item</h1>
+		</div>
+	
+		<div class="flex-stretch"></div>
+
+		<nav class="flex-none nav">
+       		<nav class="nav">
+	    		
+       		</nav>
+       	</nav>
+    </div>
+</header>
+	<div class="container">
+		<div class="card soft">
+			
+            <p>This is item # <?= $_GET['id'] ?></p>
+		</div>
+	</div>
+
+</body>
+</html>
