5f9addbaff76a7056f3f04909234a3ccb5cce691
Jfranc8 <108033774+Jfranc8@users.noreply.github.com>
2022-07-24 03:09:12 -0700

    Add files via upload

---
+++ product_list.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Product List</title>
+
+
+	
+<meta name="viewport" conttent="width=device-width">
+
+<base href="http://jessicafranco.co/aau/wnm608/franco.jessica/">
+
+<link rel="stylesheet" href="./lib/css/styleguide.css">
+<link rel="stylesheet" href="./lib/css/gridsystem.css">
+<link rel="stylesheet" href="./css/storetheme.css">
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+</head>
+<body>
+
+	<header class="navbar">
+	<div class="container display-flex">
+ 		<div class="flex-none">
+			<h1>Jessica's Indoor Houseplants</h1>
+		</div>
+		<div class="flex-stretch"></div>
+		<nav class="nav nav-flex flex-none">
+			<ul>
+				<!-- li*3>a[href=#]>{Link $} -->
+				<li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+				<li><a href="product_list.php">Shop</a></li>
+				<li><a href="product_cart.php">Cart</a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
+	<div class="container">
+		<h2>Product List</h2>
+
+			
+
+<div class='productlist grid gap'><a class="col-xs-12 col-md-4" href="product_item.php?id=1">
+    <figure class="figure product display-flex flex-column">
+        <div class="flex-stretch">
+             <img src="img/1.jpg" alt="">
+        </div>
+        <figcaption class="flex-none">
+            <div>Monstera Deliciosa</div>
+            <div>&dollar;60.00</div>
+        </figcaption>
+    </figure>
+</a><a class="col-xs-12 col-md-4" href="product_item.php?id=2">
+    <figure class="figure product display-flex flex-column">
+        <div class="flex-stretch">
+             <img src="img/2.jpg" alt="">
+        </div>
+        <figcaption class="flex-none">
+            <div>Little Yellow Fiddle</div>
+            <div>&dollar;55.00</div>
+        </figcaption>
+    </figure>
+</a><a class="col-xs-12 col-md-4" href="product_item.php?id=3">
+    <figure class="figure product display-flex flex-column">
+        <div class="flex-stretch">
+             <img src="img/3.jpg" alt="">
+        </div>
+        <figcaption class="flex-none">
+            <div>Stromanthe Triostar</div>
+            <div>&dollar;45.00</div>
+        </figcaption>
+    </figure>
+</a><a class="col-xs-12 col-md-4" href="product_item.php?id=4">
+    <figure class="figure product display-flex flex-column">
+        <div class="flex-stretch">
+             <img src="img/4.jpg" alt="">
+        </div>
+        <figcaption class="flex-none">
+            <div>ZZ Green Plant</div>
+            <div>&dollar;69.00</div>
+        </figcaption>
+    </figure>
+</a><a class="col-xs-12 col-md-4" href="product_item.php?id=5">
+    <figure class="figure product display-flex flex-column">
+        <div class="flex-stretch">
+             <img src="img/5.jpg" alt="">
+        </div>
+        <figcaption class="flex-none">
+            <div>Petite Sunset Orchid</div>
+            <div>&dollar;84.00</div>
+        </figcaption>
+    </figure>
+</a><a class="col-xs-12 col-md-4" href="product_item.php?id=6">
+    <figure class="figure product display-flex flex-column">
+        <div class="flex-stretch">
+             <img src="img/6.jpg" alt="">
+        </div>
+        <figcaption class="flex-none">
+            <div>Bromeliad Pineapple</div>
+            <div>&dollar;38.00</div>
+        </figcaption>
+    </figure>
+</a><a class="col-xs-12 col-md-4" href="product_item.php?id=7">
+    <figure class="figure product display-flex flex-column">
+        <div class="flex-stretch">
+             <img src="img/7.jpg" alt="">
+        </div>
+        <figcaption class="flex-none">
+            <div>Chinese Fan Palm</div>
+            <div>&dollar;56.00</div>
+        </figcaption>
+    </figure>
+</a><a class="col-xs-12 col-md-4" href="product_item.php?id=8">
+    <figure class="figure product display-flex flex-column">
+        <div class="flex-stretch">
+             <img src="img/8.jpg" alt="">
+        </div>
+        <figcaption class="flex-none">
+            <div>Tiny Green Pothos</div>
+            <div>&dollar;80.00</div>
+        </figcaption>
+    </figure>
+</a><a class="col-xs-12 col-md-4" href="product_item.php?id=9">
+    <figure class="figure product display-flex flex-column">
+        <div class="flex-stretch">
+             <img src="img/9.jpg" alt="">
+        </div>
+        <figcaption class="flex-none">
+            <div>Bromeliad Antonio</div>
+            <div>&dollar;45.00</div>
+        </figcaption>
+    </figure>
+</a><a class="col-xs-12 col-md-4" href="product_item.php?id=10">
+    <figure class="figure product display-flex flex-column">
+        <div class="flex-stretch">
+             <img src="img/10.jpg" alt="">
+        </div>
+        <figcaption class="flex-none">
+            <div>Fiddle Green Leaf Fig</div>
+            <div>&dollar;53.00</div>
+        </figcaption>
+    </figure>
+</a><a class="col-xs-12 col-md-4" href="product_item.php?id=11">
+    <figure class="figure product display-flex flex-column">
+        <div class="flex-stretch">
+             <img src="img/11.jpg" alt="">
+        </div>
+        <figcaption class="flex-none">
+            <div>Bromeliad Vriesea Vogue</div>
+            <div>&dollar;74.00</div>
+        </figcaption>
+    </figure>
+</a><a class="col-xs-12 col-md-4" href="product_item.php?id=12">
+    <figure class="figure product display-flex flex-column">
+        <div class="flex-stretch">
+             <img src="img/12.jpg" alt="">
+        </div>
+        <figcaption class="flex-none">
+            <div>Bromeliad Vriesea Intenso</div>
+            <div>&dollar;92.00</div>
+        </figcaption>
+    </figure>
+</a></div>	</div>
+	
+</body>
+</html>
