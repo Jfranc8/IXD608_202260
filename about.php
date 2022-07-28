5125a40b1652626ed52698be0446ee4922d699c1
Jfranc8 <108033774+Jfranc8@users.noreply.github.com>
2022-07-27 21:21:20 -0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>About Jessica</title>
+
+	
+<meta name="viewport" content="width=device-width">
+
+<base href="http://jessicafranco.co/aau/wnm608/franco.jessica/">
+
+<link rel="stylesheet" href="./lib/css/styleguide.css">
+<link rel="stylesheet" href="./lib/css/gridsystem.css">
+<link rel="stylesheet" href="./css/storetheme.css">
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script></head>
+<body>
+	
+
+<header class="navbar">
+	<div class="container display-flex">
+ 		<div class="flex-none">
+			<h1>Jessica's Indoor Houseplants</h1>
+		</div>
+		<div class="flex-stretch"></div>
+		<nav class="nav nav-flex flex-none">
+			<ul>
+				<!-- li*3>a[href=#]>{Link $} -->
+				<li><a href="index.php">Home</a></li>
+				<li><a href="product_list.php">Shop</a></li>
+				<li><a href="about.php">About</a></li>
+				<li><a href="product_cart.php">
+					<span>Cart</span>
+	    			<span class="badge"></span>
+	    		</a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
+	<div class="container">
+		<div class="card soft">
+			<h2>About Jessica's Indoor Houseplants</h2>
+
+			<p>Jessica's Indoor Houseplants is here to help you strengthen yor relationship with plants. We believe everyone should live with a little bit more green and bring the outdoors into their home. Most importantly, we focus on bringing the plants to you!</p>
+		</div>
+	</div>
+	
+</body>
+</html>
