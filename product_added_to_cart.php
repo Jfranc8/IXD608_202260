431aee041d0ddfc015aa9f36e2446c0d00515d1e
Jfranc8 <108033774+Jfranc8@users.noreply.github.com>
2022-08-02 15:29:53 -0700

    Update product_added_to_cart.php

--- product_added_to_cart.php
+++ product_added_to_cart.php
@@ -1,6 +1,14 @@
+<?php
+include_once "lib/php/functions.php";
+
+$product = makeQuery(makeConn(),"SELECT * FROM `products` WHERE `id`=".$_GET['id'])[0];
+
+$cart_product = cartItemById($_GET['id']);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,47 +16,19 @@
 	<link rel="stylesheet" href="lib/css/styleguide.css">
 	<link rel="stylesheet" href="css/storetheme.css">
 
-	
-<meta name="viewport" content="width=device-width">
-
-<base href="http://jessicafranco.co/aau/wnm608/franco.jessica/">
-
-<link rel="stylesheet" href="./lib/css/styleguide.css">
-<link rel="stylesheet" href="./lib/css/gridsystem.css">
-<link rel="stylesheet" href="./css/storetheme.css">
-
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script></head>
+	<?php include "parts/meta.php"; ?>
+</head>
 <body>
 
-	
+	<?php include "parts/navbar.php"; ?>
 
-<header class="navbar">
-	<div class="container display-flex">
- 		<div class="flex-none">
-			<h1>Jessica's Indoor Houseplants</h1>
-		</div>
-		<div class="flex-stretch"></div>
-		<nav class="nav nav-flex flex-none">
-			<ul>
-				<!-- li*3>a[href=#]>{Link $} -->
-				<li><a href="index.php">Home</a></li>
-				<li><a href="product_list.php">Shop</a></li>
-				<li><a href="about.php">About</a></li>
-				<li><a href="product_cart.php">
-					<span>Cart</span>
-	    			<span class="badge">(4)</span>
-	    		</a></li>
-			</ul>
-		</nav>
-	</div>
-</header>
 	
 	<div class="container">
 		<article id="article1" class="article card soft">
 			<div class="display-flex flex-align-center">
 				<div class="flex-stretch">
-		      		<h2>You added plant1 to your cart</h2>
-		      		<p>There are now 2 of plants1 in your cart.</p>
+		      		<h2>You added <?= $product -> name ?> to your cart</h2>
+		      		<p>There are now <?= $cart_product->amount ?> of <?= $product->name ?> in your cart.</p>
 
                     <div class="display-flex">
                     	<div class="flex-none"><a href="product_list.php">Continue Shopping</a></div>
