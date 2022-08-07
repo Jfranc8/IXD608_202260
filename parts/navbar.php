61149e92cd7a7bae1308d4b50486fdf1c0ecf3ed
Jfranc8 <108033774+Jfranc8@users.noreply.github.com>
2022-08-06 20:28:39 -0700

    Update navbar.php

--- parts/navbar.php
+++ parts/navbar.php
@@ -1,21 +1,30 @@
+<?php
 
+include_once "lib/php/functions.php";
 
+?>
+
+<input type="checkbox" id="menu" class="hidden">
 <header class="navbar">
 	<div class="container display-flex">
  		<div class="flex-none">
-	   		<h1>Jessica's Indoor Houseplants</h1>
+			<h1>Jessica's Indoor Houseplants</h1>
 		</div>
-	
 		<div class="flex-stretch"></div>
-		<nav class="flex-none nav">
-       		<nav class="nav">
-	    		<ul class="container display-flex">
-		    		<li> <a href="index.php">Home</a></li>
-		    		<li> <a href="about.php">About</a></li>
-		    		<li> <a href="product_list.php">Shop</a></li>
-		    		<li> <a href="contact.php">Contact</a></li>
-	     		</ul>
-       		</nav>
-       	</nav>
-    </div>
+		<div class="flex-none menu-button">
+			<label for="menu">&equiv;</label>
+		</div>
+		<nav class="nav nav-flex flex-none">
+			<ul>
+				<!-- li*3>a[href=#]>{Link $} -->
+				<li><a href="index.php">Home</a></li>
+				<li><a href="product_list.php">Shop</a></li>
+				<li><a href="about.php">About</a></li>
+				<li><a href="product_cart.php">
+					<span>Cart</span>
+	    			<span class="badge"><?= makeCartBadge(); ?></span>
+	    		</a></li>
+			</ul>
+		</nav>
+	</div>
 </header>
