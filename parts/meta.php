3373f71a4d0aef87b04f645d88a0de9c41779d23
Jfranc8 <108033774+Jfranc8@users.noreply.github.com>
2022-08-06 20:21:45 -0700

    Update meta.php

--- parts/meta.php
+++ parts/meta.php
@@ -1,8 +1,10 @@
 
-<meta name="viewport" content="width=device-width">
+<meta name="viewport" conttent="width=device-width">
 
 <base href="http://jessicafranco.co/aau/wnm608/franco.jessica/">
 
-<link rel="stylesheet" type="text/css" href="lib/css/styleguide.css">
-<link rel="stylesheet" type="text/css" href="lib/css/gridsystem.css">
-<link rel="stylesheet" type="text/css" href="css/storetheme.css">
+<link rel="stylesheet" href="./lib/css/styleguide.css">
+<link rel="stylesheet" href="./lib/css/gridsystem.css">
+<link rel="stylesheet" href="./css/storetheme.css">
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
