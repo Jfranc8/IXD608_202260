1b06c80802cbc8d2ebe55454bb54411e4fa83229
Jfranc8 <108033774+Jfranc8@users.noreply.github.com>
2022-08-11 09:44:21 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
+<?php
 
+include_once "lib/php/functions.php";
+include_once "parts/templates.php";
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Jessica's Store Front</title>
+	<title>Jessica's Indoor Houseplants</title>
+
+	<meta name="viewport" content="width=device-width">
 
 	<link rel="stylesheet" type="text/css" href="lib/css/styleguide.css">
+	<link rel="stylesheet" type="text/css" href="lib/css/gridsystem.css">
 	<link rel="stylesheet" type="text/css" href="css/storetheme.css">
-
-	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script></head>
-
-<body>
+</head>
+<body class="flush">
 	
 	<!-- header>h1+p -->
 	<header class="navbar">
@@ -19,45 +25,38 @@
 			<div class="flex-none font">
 				<h1>Jessica's Indoor Houseplants</h1>
 			</div>
-			<div class="flex-stretch"></div>
+		<div class="flex-stretch"></div>
 			<nav class="flex-none nav">
 				<ul class="container display-flex">
 					<li><a href="index.php">Home</a></li>
 					<li><a href="product_list.php">Shop</a></li>
 					<li><a href="about.php">About</a></li>
-					<li><a href="product_cart.php">
-						<span>Cart</span>
-						<span class="badge"></span>
-					</a></li>
+					<li><a href="product_cart.php">Cart</a></li>
 				</ul>
 			</nav>
-		</nav>
 		</div>
 	</header>
 
-	<!-- .container>article#article$.article*4>h2{Article $}+div.article-body>p*3>lorem40 --> 
-	<section class="Jessica-web-title">
+	<section class="jessica's indoor houseplants-web-title">
 		<div class="title ">
-			<h1>Welcome</h1>
+			<h3>WELCOME</h3>
+			<h1>Jessica's Indoor Houseplants</h1>
 		</div>
 	</section>
-	<div class="view-window" style="background-image: url(img/plant.jpg);">
-		<h2>INDOOR HOUSEPLANTS</h2>
+	<div class="view-window" style="background-image: url(img/plants.jpg);">
+		<h2>Plants</h2>
 	</div>
 	<div class="container">
 		<article id="article1" class="article card soft">
 			<div class="article-body">
 				<p>
-					If you are looking for indoor houseplants, we have a grown collection that provides the perfect plant for anyone and everyone. Check out our wide selection of indoor plants and make your next occassion memorable.<p>
-
-					<p>Shop now and get the plants delivered that you want for your home. We got them all!<p>
-
+					If you are looking for indoor houseplants, we have a grown collection that provides the perfect plant for anyone and everyone. Check out our wide selection of indoor plants and make your next occassion memorable.
 
 				</p>
 			</div>
 		</article>
 	<div class="view-window" style="background-image: url(img/background.jpg);" width="100%">
-		<h2>DESIGNED FOR YOUR HOME</h2>
+		<h2>DESIGNED  FOR YOUR HOME</h2>
 	</div>
 
 	<div class="container">
@@ -65,9 +64,14 @@
 			<div class="article-body">
 				<p>
 					The perfect plant for your own living space. 
-				</p>
 			</div>
 		</article>
+	</div>
+	<div class="container">
+		<h2>Latest Abstract</h2>
+		<?php recommendedCategory("abstract"); ?>
+		<h2>Latest Digital</h2>
+		<?php recommendedCategory("digital"); ?>
 	</div>
 
 </body>
