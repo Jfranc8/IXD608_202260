e547b27370f04fb0b9dd7528459157345c2f488c
Jfranc8 <108033774+Jfranc8@users.noreply.github.com>
2022-08-11 09:28:28 -0700

    Update about.php

--- about.php
+++ about.php
@@ -1,51 +1,23 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>About Jessica</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>About Jessica's Indoor Houseplants</title>
 
-	
-<meta name="viewport" content="width=device-width">
-
-<base href="http://jessicafranco.co/aau/wnm608/franco.jessica/">
-
-<link rel="stylesheet" href="./lib/css/styleguide.css">
-<link rel="stylesheet" href="./lib/css/gridsystem.css">
-<link rel="stylesheet" href="./css/storetheme.css">
-
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script></head>
+<?php include "parts/meta.php"; ?>
+</head>
 <body>
-	
+<?php include "parts/navbar.php"; ?>
 
-<header class="navbar">
-	<div class="container display-flex">
- 		<div class="flex-none">
-			<h1>Jessica's Indoor Houseplants</h1>
-		</div>
-		<div class="flex-stretch"></div>
-		<nav class="nav nav-flex flex-none">
-			<ul>
-				<!-- li*3>a[href=#]>{Link $} -->
-				<li><a href="index.php">Home</a></li>
-				<li><a href="product_list.php">Shop</a></li>
-				<li><a href="about.php">About</a></li>
-				<li><a href="product_cart.php">
-					<span>Cart</span>
-	    			<span class="badge"></span>
-	    		</a></li>
-			</ul>
-		</nav>
-	</div>
-</header>
-	<div class="container">
-		<div class="card soft">
-			<h2>About Jessica's Indoor Houseplants</h2>
+<div class="container">
+<div class="card soft">
+<h2>About Jessica's Indoor Houseplants</h2>
 
-			<p>Jessica's Indoor Houseplants is here to help you strengthen yor relationship with plants. We believe everyone should live with a little bit more green and bring the outdoors into their home. Most importantly, we focus on bringing the plants to you!</p>
-		</div>
-	</div>
-	
+<p>This is information about Jessica
+</p>
+</div>
+</div>
+
 </body>
 </html>
