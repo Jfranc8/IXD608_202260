1118cf671325dcf6e0256a05fac9d5b13bd1d8d5
Jfranc8 <108033774+Jfranc8@users.noreply.github.com>
2022-08-17 07:26:42 -0700

    Add files via upload

---
+++ franco.jessica/lib/php/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+function MySQLIAuth() {
+	return [
+		"localhost", // mysql host
+		"JFWNM608", // mysql user name
+		"Test@123", //mysql user password
+		"JFWNM608", // mysql database name
+
+	];
+}
